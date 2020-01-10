d07b9fb8d90e6d93dc570befc4b3a29d5e6b6794
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-11 06:20:02 +0700

    Using Normal CSS Grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = props => {
@@ -10,7 +11,7 @@ const ImageList = props => {
   //   return <img alt={image.description} key={image.id} src={image.urls.regular} />;
   // });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
