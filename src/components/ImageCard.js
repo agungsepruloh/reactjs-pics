1b5984e5be2378d4896e0ac0199062524e798e9c
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-11 07:50:46 +0700

    Dynamic Spans Implementation

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,7 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -11,20 +12,22 @@ class ImageCard extends React.Component {
     // console.log(this.imageRef);
     this.imageRef.current.addEventListener("load", this.setSpans);
 
-    // but if we console log the actual client heigh, it will show 0. What's happen?
+    // but if we console log the actual client height, it will show 0. What's happen?
     // the reason is beacuse it's too early, because the image isn't rendered yet into the DOM
     // console.log(this.imageRef.current.clientHeight);
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 5 + 1);
+    this.setState({ spans: spans });
   };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />;
       </div>
     );
