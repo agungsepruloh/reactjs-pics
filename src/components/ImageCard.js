ede71e80d1f6ab8e9dab82a1ed913ede181ffb86
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-11 07:11:04 +0700

    Access the Image Height and Callbacks on Image Load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // after the thing is rendered
+    // console.log(this.imageRef);
+    this.imageRef.current.addEventListener("load", this.setSpans);
+
+    // but if we console log the actual client heigh, it will show 0. What's happen?
+    // the reason is beacuse it's too early, because the image isn't rendered yet into the DOM
+    // console.log(this.imageRef.current.clientHeight);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
       <div>
-        <img alt={description} src={urls.regular} />;
+        <img ref={this.imageRef} alt={description} src={urls.regular} />;
       </div>
     );
   }
